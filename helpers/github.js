594f69706a13f65651b358740ddc8b936379a357
Josh Klein <jGlass314@gmail.com>
2018-01-03 12:11:50 -0800

    modified api helper to take environment variable api key

--- helpers/github.js
+++ helpers/github.js
@@ -1,5 +1,5 @@
 const request = require('request-promise');
-const config = require('../config.js');
+let config;
 
 let getReposByUsername = (/* TODO */username) => {
   // TODO - Use the request module to request repos for a specific
@@ -7,11 +7,19 @@ let getReposByUsername = (/* TODO */username) => {
   
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
+  try {
+    config = require('../config.js');
+  } catch (e) {
+    console.log('config.js not found');
+  }
+
+  const token = (config && config.TOKEN ? config.TOKEN : process.env.API_KEY);
+
   let options = {
     url: 'https://api.github.com/users/' + username + '/repos',
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'Authorization': `token ${token}`
     }
   };
   // console.log('gonna try to getReposByUsername with options:', options);
